3e77716897850ba1b8847a874b499bd5a92ef28a
Lucas.Bechtlufft12 <80471255+LucasBecht@users.noreply.github.com>
2021-05-19 12:03:29 -0300

    Update Crawler.py

--- Crawler.py
+++ Crawler.py
@@ -12,7 +12,7 @@ results = requests.get(url)
 
 soup = BeautifulSoup(results.text, "html.parser")
 
-# Storage
+# Storage, this is just a template to how i think i'll separate the information, may change.
 brand = []
 product = []
 price = []
@@ -21,5 +21,7 @@ ddr = []
 availability = []
 
 # After looking at the product page using the "Inspect" command, i've noticed that every product is from the div class "sc-fzqARJ eITELq", so i set the soup.find_all to get
-# all the divs with that specific class, which then gets saved into products_div
+# all the divs with that specific class, which then gets saved into products_div,
+# the problem now is that, this returns nothing, so i'm either choosing the wrong tag to get the information, or i am not getting the information in the first place.
+# testing now to see which one it is.
 products_div = soup.find_all('div', class_='sc-fzqARJ eITELq')
